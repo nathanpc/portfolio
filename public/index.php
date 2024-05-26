cccce96167df7516dc342ce13aba9cdc9d79ce3d
Nathan Campos <nathan@innoveworkshop.com>
2024-05-26 11:26:01 +0100

    Added a small biography to the front page

--- public/index.php
+++ public/index.php
@@ -42,11 +42,42 @@ require_once __DIR__ . '/../src/navbar.php';
 		<div id="content">
 			<div class="section">
 				<h2>about me</h2>
-				<p>This should be a nice explanation about me and should
-				sumamrize things that I make and made in the past. It's very
-				important that this showcases everything that I am and show
-				how I can help people and companies out with my knowledge and
-				expertise.</p>
+
+				<p>I'm a <b>full-stack developer</b> (although I prefer the
+				backend), <b>creative technologist</b> and an <b>electronics
+				engineer</b> born in <a
+				href="https://en.wikipedia.org/wiki/Brazil">Brazil</a> and
+				currently living in <a
+				href="https://en.wikipedia.org/wiki/Portugal">Portugal</a>.
+				You can find me working on almost anything that involves
+				electricity, both in terms of hardware and software. Building
+				things has always been a passion of mine, so you'll notice that
+				I'm always self-motivated to work on new projects that involve
+				technology. Some of the things that I've worked on have been
+				used by hundreds of thousands of people, maybe into the millions
+				depending on who's counting, and my work has definitely reached
+				at least tens of millions.</p>
+
+				<p>I'm currently dedicating some of my time to passing some of
+				my knowledge to another generation and having a lot of fun
+				working on creative projects with my students and colleagues at
+				<a href="https://www.iade.europeia.pt/">IADE - Creative
+				University</a>. I'm mostly involved with the <a
+				href="https://www.iade.europeia.pt/en/undergraduate-programs/bachelor-creative-technologies/">
+				Creative Technologies</a>, <a
+				href="https://www.iade.europeia.pt/en/undergraduate-programs/bachelor-games-development/">
+				Games Development</a>, and <a
+				href="https://www.iade.europeia.pt/licenciaturas/engenharia-informatica/">
+				Computer Science</a> degrees.</p>
+
+				<p>If you browse this website, <a
+				href="https://github.com/nathanpc">my GitHub profile</a>, or <a
+				href="<?= $_SERVER['REQUEST_SCHEME'] . '://innoveworkshop.com/'
+				?>">my company's website</a> you'll notice that I'm capable of
+				building quite a lot of amazing things. If you're looking for
+				someone with my skillset either for your company or to make your
+				idea become a reality feel free to reach out to me at <?=
+				safe_email('nathan@innoveworkshop.com') ?>.</p>
 			</div>
 		</div>
 
