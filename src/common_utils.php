5980c0ec927de90b65d6cecef18cfb5b994087ee
Nathan Campos <nathan@innoveworkshop.com>
2024-05-26 11:12:48 +0100

    Created a simple function to help wrap emails
    This function should make emails less succeptible to crawlers, but I've
    decided to ignore that and just make it help us style emails.

--- src/common_utils.php
+++ src/common_utils.php
@@ -17,3 +17,18 @@
 function href(string $loc): string {
 	return $loc;
 }
+
+/**
+ * Builds up an HTML element that makes it safe to publicly share an email
+ * address on the internet. This will only keep you safe from the shittiest of
+ * spam crawlers.
+ *
+ * @param $email Email to be "machine-obfuscated".
+ *
+ * @return A safer way to share an email on the open web.
+ */
+function safe_email(string $email): string {
+	$parts = explode('@', $email, 2);
+	return "<a class=\"email\" href=\"mailto:$email\">{$parts[0]} [at] " .
+		"{$parts[1]}</a>";
+}
