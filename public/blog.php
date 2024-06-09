7f03cebffb12f20b94d8a287b9faaa57ff6cecb6
Nathan Campos <nathan@innoveworkshop.com>
2024-06-09 16:32:19 +0100

    Moved blog functions to its own module and refactored into class
    In order to make things a bit more manageable in the forseeable future
    the blog-related functions were moved to their own module and everything
    related to them was refactored into a class in order to allow for more
    flexibility down the line.

--- public/blog.php
+++ public/blog.php
@@ -1,91 +1,31 @@
 <?php
 require_once __DIR__ . '/../templates/includes.php';
-
-/**
- * Builds the blog post file path.
- *
- * @param ?string $date Date when the blog post was created.
- * @param ?string $slug Blog post slug.
- *
- * @return ?string Possible path to the blog post source file or NULL if the
- *                 supplied parameters are invalid.
- */
-function get_post_path(string $date = null, string $slug = null): string|null {
-	$date = $date ?? $_GET['date'];
-	$slug = $slug ?? $_GET['slug'];
-
-	// Sanitize inputs.
-	$safe_date = preg_replace('/[^0-9\-]/', '', $date);
-	$safe_slug = preg_replace('/[^0-9a-zA-Z\-_]/', '', $slug);
-
-	// Ensure we always fail when someone tries to be cleaver.
-	if (($date != $safe_date) || ($slug != $safe_slug))
-		return null;
-
-	// Check if the file actually exists.
-	$fpath = __DIR__ . "/../blog/{$safe_date}_{$safe_slug}.php";
-	if (!file_exists($fpath))
-		return null;
-
-	return $fpath;
-}
-
-/**
- * Gets the requested blog post structure.
- *
- * @return ?array Associative array with the information and contents of the
- *                blog post.
- */
-function get_blog_post(): array|null {
-	// Get the post's path.
-	$path = get_post_path();
-	if (is_null($path))
-		return null;
-
-	// Load and buffer the contents of the blog post.
-	ob_start();
-	include_once($path);
-	$post['content'] = ob_get_contents();
-	ob_end_clean();
-	$post['slug'] = preg_replace('/[^0-9a-zA-Z\-_]/', '', $_GET['slug']); 
-	$post['date'] = date_create(preg_replace('/[^0-9\-]/', '', $_GET['date']));
-	$post['last_modified'] = filemtime($path);
-
-	return $post;
-}
-
-/**
- * Gets the permalink to the blog post.
- *
- * @param array $post Blog post structure.
- *
- * @return string Permalink to the blog post.
- */
-function post_permalink(array $post): string {
-	return href('/blog/' . date('Y-m-d', $post['date']->getTimestamp()) .
-		"/{$post['slug']}");
-}
+require_once __DIR__ . '/../src/blog.php';
 
 // Get the blog post.
-$post = get_blog_post();
+$post = BlogPost::FromRequest();
 
 // Ensure that the last modified date displayed is of the post's file.
 $last_modified = null;
 if (!is_null($post))
-	$last_modified = $post['last_modified'];
+	$last_modified = $post->last_modified;
 
 // Fix the breadcrumbs bar.
-$crumbs = array(
-	'blog' => '/blog',
-	$post['slug'] => post_permalink($post)
-);
+$crumbs = array('blog' => '/blog');
+if (!is_null($post)) {
+	$crumbs = array(
+		'blog' => '/blog',
+		$post->slug => $post->permalink()
+	);
+}
 
 // Render the DOCTYPE template.
 include_once __DIR__ . '/../templates/doctype.php';
 ?>
 <html>
 <head>
-	<title><?= is_null($post) ? 'Post not found' : $post['title'] ?></title>
+	<title><?= !is_null($post) ? $post->meta('title') :
+		'Post not found' ?></title>
 	
 	<?php include_once __DIR__ . '/../templates/head.php'; ?>
 </head>
@@ -95,14 +35,12 @@ include_once __DIR__ . '/../templates/doctype.php';
 	<?php if (!is_null($post)) { ?>
 		<!-- Blog post header. -->
 		<div id="blog-post" class="section">
-			<h2><?= $post['title'] ?>
-			<a href="<?= post_permalink($post) ?>">#</a></h2>
-			<div id="published-date">
-				<?= date('Y-m-d', $post['date']->getTimestamp()) ?>
-			</div>
+			<h2><?= $post->meta('title') ?>
+			<a href="<?= $post->permalink() ?>">#</a></h2>
+			<div id="published-date"><?= $post->published_date() ?></div>
 		</div>
 		
-		<?= $post['content'] ?>
+		<?= $post->content() ?>
 	<?php } else { ?>
 		<?php http_response_code(404); ?>
 		<div class="section">
---
+++ src/blog.php
@@ -0,0 +1,141 @@
+<?php
+/**
+ * blog.php
+ * Handles everything related to the blog and its entries.
+ * 
+ * @author Nathan Campos <nathan@innoveworkshop.com>
+ */
+
+require_once __DIR__ . '/common_utils.php';
+
+/**
+ * Blog post abstraction.
+ */
+class BlogPost {
+	public string $slug;
+	public DateTime $date;
+	public int $last_modified;
+	public ?array $metadata = null;
+	public ?string $content = null;
+	public ?string $path = null;
+
+	public function __construct(string $slug, DateTime $date) {
+		$this->slug = $slug;
+		$this->date = $date;
+		$this->last_modified = $date->getTimestamp();
+	}
+
+	/**
+	 * Gets a blog post object from a request.
+	 *
+	 * @return ?BlogPost Requested blog post object or NULL if it wasn't found.
+	 */
+	public static function FromRequest(): ?BlogPost {
+		return self::FromDateSlug($_GET['date'], $_GET['slug']);
+	}
+
+	/**
+	 * Gets a blog post object from a date and slug.
+	 *
+	 * @return ?BlogPost Requested blog post object or NULL if it wasn't found.
+	 */
+	public static function FromDateSlug(string $date, string $slug): ?BlogPost {
+		// Get the file and check if it actually exists.
+		$fpath = self::get_path($date, $slug);
+		if (!file_exists($fpath))
+			return null;
+
+		// Build up the post object.
+		$post = new self($slug, date_create($date));
+		$post->set_path($fpath);
+
+		return $post;
+	}
+
+	/**
+	 * Gets the permalink to the blog post.
+	 *
+	 * @return string Permalink to the blog post.
+	 */
+	public function permalink(): string {
+		return href('/blog/' . date('Y-m-d', $this->date->getTimestamp()) .
+			"/{$this->slug}");
+	}
+
+	/**
+	 * Gets the published date as a pretty ISO8601 string.
+	 *
+	 * @return string Published date in ISO8601 format.
+	 */
+	public function published_date(): string {
+		return date('Y-m-d', $this->date->getTimestamp());
+	}
+
+	/**
+	 * Get metadata information from the post.
+	 *
+	 * @param string $key Metadata key.
+	 * 
+	 * @return any Requested information.
+	 */
+	public function meta(string $key) {
+		// Read the metadata in case we haven't cached it previously.
+		if (is_null($this->metadata))
+			$this->content();
+	
+		return $this->metadata[$key];
+	}
+
+	/**
+	 * Gets the post's content.
+	 *
+	 * @return string The associated post's content.
+	 */
+	public function content(): string {
+		// Check if first we need to fetch the content.
+		if (is_null($this->content)) {
+			// A weird post without an associated file.
+			if (is_null($this->path))
+				return $this->content;
+
+			// Fetch the content and cache it.
+			ob_start();
+			include_once($this->path);
+			$this->metadata = $post;
+			$this->content = ob_get_contents();
+			ob_end_clean();
+		}
+
+		return $this->content;
+	}
+
+	/**
+	 * Sets the associated file path.
+	 *
+	 * @param string $fpath File path to be associated with this post.
+	 */
+	private function set_path(string $fpath) {
+		$this->path = $fpath;
+		$this->last_modified = filemtime($this->path);
+	}
+
+	/**
+	 * Builds the blog post file path.
+	 *
+	 * @param string $date Date when the blog post was created.
+	 * @param string $slug Blog post slug.
+	 *
+	 * @return ?string Possible path to the blog post source file.
+	 */
+	private static function get_path(string $date, string $slug): string|null {
+		// Sanitize inputs.
+		$safe_date = preg_replace('/[^0-9\-]/', '', $date);
+		$safe_slug = preg_replace('/[^0-9a-zA-Z\-_]/', '', $slug);
+
+		// Ensure we always fail when someone tries to be cleaver.
+		if (($date != $safe_date) || ($slug != $safe_slug))
+			return null;
+
+		return realpath(__DIR__ . "/../blog/{$safe_date}_{$safe_slug}.php");
+	}
+}
