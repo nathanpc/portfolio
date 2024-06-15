7463ce1da5c971f57ca534c722a494066719ca4b
Nathan Campos <nathan@innoveworkshop.com>
2024-06-15 15:37:49 +0100

    Added year separators to blog listing

--- public/blog.php
+++ public/blog.php
@@ -50,10 +50,19 @@ include_once __DIR__ . '/../templates/doctype.php';
 
 	<?php if ($listing) { ?>
 		<!-- Blog listing. -->
+		<?php $last_year = ""; ?>
 		<?php foreach ($listing as $post_json) { ?>
 			<?php $post = BlogPost::FromJSON($post_json); ?>
+
+			<?php if ($last_year != $post->date->format('Y')) { ?>
+				<!-- Year marker. -->
+				<?php $last_year = $post->date->format('Y'); ?>
+				<h3 class="post-year"><?= $last_year ?></h3>
+			<?php } ?>
+
 			<div class="post-listing">
-				<span class="published-date"><?= $post->published_date() ?></span>
+				<span class="published-date"><?= $post->published_date()
+					?></span>
 				<a class="title" href="<?= $post->permalink() ?>">
 					<?= $post->title() ?>
 				</a>
