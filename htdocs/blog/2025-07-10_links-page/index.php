133529472973ff759fb909a0ba5bd4c4696b892c
Nathan Campos <nathan@innoveworkshop.com>
2025-07-10 10:46:09 +0100

    [blog] Hoarding links and creating a directory of internet knowledge

---
+++ htdocs/blog/2025-07-10_links-page/index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+	<?php include __DIR__ . '/../../../templates/head.php'; ?>
+
+	<!-- Page information. -->
+	<title>Hoarding links and creating a directory of internet knowledge</title>
+</head>
+<body>
+	<?php include_template('header'); ?>
+
+	<div id="blog-post" class="section">
+		<h2>Hoarding links and creating a directory of internet knowledge</h2>
+		<div id="published-date">2025-07-10</div>
+	</div>
+
+	<p>
+		Over the years, I've visited many websites while on a hunt for
+		difficult-to-find information about obscure or simply obsolete things.
+		As I found the information I wanted I simply absorbed that knowledge and
+		moved on. This behaviour made me extremely knowledgeable on a bunch of
+		things, but has left me with no history, and no way to share my
+		journeys.
+	</p>
+
+	<p>
+		Specially as I get older, I'm realizing that many of the things I once
+		knew by heart have faded away and need refreshing. Searching for some of
+		these topics again has sometimes not yielded the same results, both due
+		to the <a href="https://en.wikipedia.org/wiki/Enshittification">enshittification</a>
+		of search engine results, but also due to some of the websites that
+		contained such information going offline and no longer available in the
+		search engine indexes. This has lead me to great frustration, as I took
+		for granted that the internet was forever and that information would
+		always be available.
+	</p>
+
+	<p>
+		This week this cycle has repeated itself as I needed some information
+		on how MS-DOS's disk management works and a website that I vividly
+		remember containing everything I needed in great detail is nowhere to be
+		found.
+	</p>
+
+	<p>
+		To finally break this cycle I have decided that it's about time I get up
+		from my lazy ass and build a simple page to catalog everything I find of
+		interest while on my internet journeys. Inspired by links pages from
+		other members of the community such as <a href="https://box.matto.nl/links.html">matto</a>
+		and <a href="https://sizeof.cat/links/">sizeof(cat)</a>, I decided that
+		it was about time that this website got <a href="<?= href('/links') ?>">its own links page</a>
+		as well.
+	</p>
+
+	<p>
+		The goal of this page is to provide a living directory of the internet's
+		knowledge as I stumble upon new topics or as I'm researching and deep
+		diving into a specific topic or project. A curated, categorized, and
+		commented selection of links that contain valuable information about any
+		given topic of my interest.
+	</p>
+
+	<p>
+		I imagine the page will grow exponentially and become a mess, but at
+		least the information will be there, indexable, and if I'm diligent,
+		every page will be backed up to the <a href="https://web.archive.org">Internet Archive</a>.
+		Let's help preserve the internet's knowledge!
+	</p>
+
+	<?php include_template('footer'); ?>
+</body>
+</html>
